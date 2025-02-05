8a9a07e2eb206f3d6a9d97c18bfe093dd1ba8a1d
Jake Huang <zijianhuang03@gmail.com>
2025-02-05 12:11:07 -0500

    Initial Commit

---
+++ silentMeetingTimer Watch App/silentMeetingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  silentMeetingTimerApp.swift
+//  silentMeetingTimer Watch App
+//
+//  Created by Jake Huang on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct silentMeetingTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ silentMeetingTimer Watch AppTests/silentMeetingTimer_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  silentMeetingTimer_Watch_AppTests.swift
+//  silentMeetingTimer Watch AppTests
+//
+//  Created by Jake Huang on 2/5/25.
+//
+
+import Testing
+@testable import silentMeetingTimer_Watch_App
+
+struct silentMeetingTimer_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
